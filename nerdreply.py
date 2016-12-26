24b1e240158830e2f01fca2ecb5847f51329b24d
Brobot <Brobot@bro.com>
2016-12-26 21:39:21 +0000

    Minimum viable product working, sorta

--- nerdreply.py
+++ nerdreply.py
@@ -1,24 +1,56 @@
 #!/usr/bin/python
-from ircbotbase import *
 import os
 import random
+import re
 import bangers as bng
+import pdb
+from telnetlib import Telnet
 
 channel = "&bitlebee"
-server = "localhost"
 nickname = "brobot"
 realname = "Bro"
 regpass = os.environ["IRCPASSWORD"]
 fbchan = "#the"
 
-irc = IRC()
-irc.connect(server, channel, nickname, realname, regpass, fbchan)
- 
-while True:
-    text = irc.get_text()
-    print text
- 
-    if "PRIVMSG" in text and channel in text and "nerds" in text:
-        irc.send(channel, "nerds")
-    elif "PRIVMSG" in text and channel in text and "bang" in text.lower():
-        irc.send(channel, bng.handle_response(text))
+def sendMsg(tn, msg):
+    print "DEBUG: Sending msg=" + msg
+    tn.write("PRIVMSG " + fbchan + " " + msg + "\n")
+
+def cleanup(msg):
+    return msg.split("PRIVMSG " + fbchan + " :")[1]
+
+def telnetMain():
+    print "DEBUG: Opening telnet handle"
+    tn = Telnet("localhost", 6667)
+    tn.set_debuglevel(5)
+    tn.read_until("BitlBee-IRCd initialized, please go on")
+
+    tn.write("NICK brobot\n")
+    tn.write("USER brobot 8 *: Alex\n")
+    tn.read_until("identify yourself", 3)
+
+    print "DEBUG: Joining bitlbee"
+    tn.write("JOIN &bitlbee\n")
+    tn.write("PRIVMSG &bitlbee :identify " + regpass + "\n")
+    tn.read_until("facebook - Logging in: Logged in", 3)
+
+    print "DEBUG: Joining channel"
+    tn.write("JOIN " + fbchan + "\n")
+
+    print "DEBUG: Telnetmain finished"
+
+    expressions = [".*nerd.*\r\n", ".*bang.*\r\n"]
+    while True:
+        (idx, match, output) = tn.expect(expressions)
+        print "DEBUG: idx=" + str(idx)
+        print "DEBUG: match=" + match.group(0)
+        print "DEBUG: cleanedUp=" + cleanup(match.group(0))
+        #print "DEBUG: output=" + output
+        if idx == 0:
+            sendMsg(tn, 'Yup. Bro')
+        if idx == 1:
+            sendMsg(tn, "Holdmahbeer.")
+            sendMsg(tn, bng.handle_response(cleanup(match.group(0))))
+
+if __name__ == '__main__':
+    telnetMain()
