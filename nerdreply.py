eb7c73cc0e5dd72d32f5bdfaf13ede77d862d083
C-Hipple <Chris.Hipple@gmail.com>
2016-12-29 13:54:15 -0500

    brought updated nerdreply from master

--- nerdreply.py
+++ nerdreply.py
@@ -3,9 +3,9 @@ import os
 import random
 import re
 import bangers as bng
+import weather as wthr
 import pdb
 from telnetlib import Telnet
-from initDatabase import createDB
 
 channel = "&bitlebee"
 nickname = os.environ["NICKNAME"]
@@ -18,14 +18,10 @@ def sendMsg(tn, msg):
     if not msg:
         return
     print "DEBUG: Sending msg=" + msg
-    tn.write("PRIVMSG " + fbchan + " :" + msg + "\n")
+    tn.write(("PRIVMSG " + fbchan + " :" + msg + "\n").encode('utf-8'))
 
 def cleanup(msg):
     return msg.split("PRIVMSG " + fbchan + " :")[1]
-
-def grabName(msg):
-    '''name is between the : and the ! on the match string.'''
-    return msg.split(':')[1].split('!')[0]
 
 def telnetMain():
     print "DEBUG: Opening telnet handle"
@@ -33,8 +29,8 @@ def telnetMain():
     tn.set_debuglevel(5)
     tn.read_until("BitlBee-IRCd initialized, please go on")
 
-    tn.write("NICK brobot\n")
-    tn.write("USER brobot 8 *: Alex\n")
+    tn.write("NICK " + nickname +"\n")
+    tn.write("USER " + username + " 8 *: " + realname + "\n")
     tn.read_until("identify yourself", 3)
 
     print "DEBUG: Joining bitlbee"
@@ -47,8 +43,7 @@ def telnetMain():
 
     print "DEBUG: Telnetmain finished"
 
-
-    expressions = [".*nerd.*\r\n", ".*bang.*\r\n"]
+    expressions = [".*nerd.*\r\n", ".*bang.*\r\n", ".*philly*.\r\n", ".*nyc*.\r\n"]
     while True:
         (idx, match, output) = tn.expect(expressions)
         print "DEBUG: idx=" + str(idx)
@@ -58,21 +53,9 @@ def telnetMain():
         if idx == 0:
             sendMsg(tn, "nerd")
         if idx == 1:
-            sendMsg(tn, bng.handle_response(cleanup(match.group(0)),
-                                            grabName(match.group(0))))
+            sendMsg(tn, bng.handle_response(cleanup(match.group(0))))
+        if idx == 2 or idx == 3:
+            sendMsg(tn, wthr.handle_response(cleanup(match.group(0))))
 
 if __name__ == '__main__':
-    # Check to make sure database exists
-    if 'iBangersBotDB.sqlite3' not in os.listdir(os.getcwd()):
-        createDB()
-        with open('bangers.txt') as f:
-            for banger in f.readlines():
-                try:
-                    add_banger(banger, 5, False)
-                except Exception as e:
-                    print(e)
-                    print('Banger already added')
-
-
-    # Run the bot
     telnetMain()
