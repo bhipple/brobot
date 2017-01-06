940acd6c76b428375e99c7105facc8fab335584c
agracie <alexandergracie17@gmail.com>
2017-01-06 07:03:40 +0000

    locq/darksky integration

---
+++ d20.py
@@ -0,0 +1,6 @@
+import random
+
+
+def roll_d20:
+    roll = random.randint(0,20)
+    print roll
--- weather.py
+++ weather.py
@@ -1,37 +1,50 @@
 import json
 import urllib
 import os
+import requests
 
 dskey = os.environ["DARKSKYKEY"]
+lociq = "4257610dd0aadeaaece4"
+locurl = "http://locationiq.org/v1/search.php?key="
+locvar = "&limit=1&countrycodes=US&format=json&q="
 
-#Need to fix check to if not nyc and not philly prompt again
+
+text = raw_input()
+
 
 def handle_response(text):
-    if 'philly' in text.lower():
-        return philly_weather()
-    elif 'nyc' in text.lower():
-        return nyc_weather()
-    elif 'philly' or 'nyc' not in text.lower():
-        print "philly or nyc?"
-    else:
-        return 
+#    for text in text.lower():
+#       return coord_get()
+    for text in text.lower():   
+       return weather()
+
+def coord_get(): 
+    r = requests.get(locurl + lociq + locvar + text )  
+    parsed_json = json.loads(r.text)
+    for i in parsed_json:
+
+         print "DEBUG coord_get"
+         return i['lat'], i['lon'], i['display_name']
+
+def just_coord(): 
+    r = requests.get(locurl + lociq + locvar + text )  
+    parsed_json = json.loads(r.text)
+    for i in parsed_json:
+         print "DEBUG just_coord"
+         return i['lat'] + ',' +  i['lon']
 
 
-def philly_weather():
-
-    url = "https://api.darksky.net/forecast/" + dskey + "/39.95,-75.16"
+def weather():
+    coord = str(just_coord())
+    print "DEBUG weather"
+    coord =  coord.translate(None, ' u()')
+    url = "https://api.darksky.net/forecast/" + dskey + "/" + coord
     response = urllib.urlopen(url)
     parsed_json = json.loads(response.read())
+    return(parsed_json['daily']['summary'])
     
-    return(parsed_json['daily']['summary'])
 
-def nyc_weather():
+print (handle_response(text))
 
-    url = "https://api.darksky.net/forecast/" + dskey + "/40.7128,-74.0059"
-    response = urllib.urlopen(url)
-    parsed_json = json.loads(response.read())
-
-    return(parsed_json['daily']['summary'])
-
-if __name__ == '__main__':
-    print(handle_response())
+#if __name__ == '__main__':
+#    print(handle_response())
