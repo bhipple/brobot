1cd72a91611c1a698b0de0e8266f359d5064ec90
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-01-23 22:49:35 -0500

    Cleanup nix file

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
     name = "py";
-    buildInputs = [ stdenv python27Full python27Packages.requests python27Packages.pyflakes ];
+    buildInputs = [ stdenv
+                    python27Full
+                    python27Packages.coveralls
+                    python27Packages.requests
+                    python27Packages.pyflakes
+                  ];
   };
 }
