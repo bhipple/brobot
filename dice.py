db73fdc095d473a5e3177195207c9d4f96ec1889
C-Hipple <Chris.Hipple@gmail.com>
2017-02-09 17:08:56 -0500

    started dice parsing

--- dice.py
+++ dice.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 def rollin():
     return random.randint(1, 20)
@@ -10,3 +11,13 @@ def rollOff(text):
         rolls += [(rollin(), name)]
     res = map(lambda (r, p): p + ": " + str(r), sorted(rolls, reverse=True))
     return "\r\n".join(res)
+
+
+def rollDice(text):
+    """matches xdy from input text and rolls that x dice of y size"""
+    match = re.match("?P([0-9])(d[0-9])", text)
+    return match.group(0)
+
+
+if __name__ == "__main__":
+    print(rollDice("2d6"))
